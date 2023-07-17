cedffbdb50b1df050fc7d770ce065b113b4b0596
w3villa-anshul-yadav <anshul.yadav@w3villa.com>
2023-07-17 12:46:51 +0530

    logs nyc output file added in gitignore

--- DB/DBConnection.js
+++ DB/DBConnection.js
@@ -1,29 +1,28 @@
-const mongoose = require("mongoose");
-const logger = require("../logger");
-const DBConfig = require("../config/DBConfig");
+const mongoose = require('mongoose');
+const logger = require('../logger');
+const DBConfig = require('../config/DBConfig');
 
 const connectDB = async () => {
     // Test Environment
-    if (process.env.NODE_ENV === "test") {
+    if (process.env.NODE_ENV === 'test') {
         const host = DBConfig.test.HOST;
         const port = DBConfig.test.PORT;
         const db = DBConfig.test.DB;
         try {
-            await mongoose.connect(host + ":" + port + "/" + db);
-            logger.info("Connected to database");
+            await mongoose.connect(host + ':' + port + '/' + db);
+            logger.info('Connected to database');
         } catch (error) {
             logger.error(error);
         }
     }
     // Test Environment
-    if (process.env.NODE_ENV === "development") {
-        console.log("here", process.env.NODE_ENV);
+    if (process.env.NODE_ENV === 'development') {
         const host = DBConfig.development.HOST;
         const port = DBConfig.development.PORT;
         const db = DBConfig.development.DB;
         try {
-            await mongoose.connect(host + ":" + port + "/" + db);
-            logger.info("Connected to database");
+            await mongoose.connect(host + ':' + port + '/' + db);
+            logger.info('Connected to database');
         } catch (error) {
             logger.error(error);
         }
